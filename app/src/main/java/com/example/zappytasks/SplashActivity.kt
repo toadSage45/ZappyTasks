0e4943e320d56ab4e72c85c29f44617e9058d39a
toadSage45 <alok.20223026@mnnit.ac.in>
2025-06-21 16:15:12 +0530

    Setup splash screen

---
+++ app/src/main/java/com/example/zappytasks/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.zappytasks
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "ShortBaby-Mg2w.ttf")
+        val tvAppName = findViewById<TextView>(R.id.tv_app_name)
+        tvAppName.typeface = typeFace
+
+    }
+}
